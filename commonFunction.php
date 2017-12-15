35976cb198d8f66a0384c4debd368193e9dbc60c
jia27222 <34232159+jia27222@users.noreply.github.com>
2017-12-15 13:29:27 +0800

    Update commonFunction.php

--- commonFunction.php
+++ commonFunction.php
@@ -6,3 +6,16 @@ function creatID(){
    $ID =  strftime("%y%m%d" ,time()).(intval(time()) -strtotime(date('Y-m-d',time()))).mt_rand(1000,9999);
    return $ID;
 }
+
+//对象转数组
+function object2array($object) {
+    if (is_object($object)) {
+        foreach ($object as $key => $value) {
+            $array[$key] = $value;
+        }
+    }
+    else {
+        $array = $object;
+    }
+    return $array;
+}
