19765ac8d01820dd3db5b7b06f03619a4bb30996
jia27222 <34232159+jia27222@users.noreply.github.com>
2018-06-08 14:44:49 +0800

    Update commonFunction.php

--- commonFunction.php
+++ commonFunction.php
@@ -60,3 +60,30 @@ function creatID(){
     
          return $time;
      }
+            
+     /**
+     * 接收Base64的图片上传
+     */
+    public function upload2(){
+        $base64_image_content = $_POST['imgBase64'];
+        //匹配出图片的格式
+        if (preg_match('/^(data:\s*image\/(\w+);base64,)/', $base64_image_content, $result)){
+            $type = $result[2];
+            $new_file = "./Public/Cover/".date('Ymd',time())."/";
+            if(!file_exists($new_file))
+            {
+                //检查是否有该文件夹，如果没有就创建，并给予最高权限
+                mkdir($new_file, 0700);
+            }
+            $new_file = $new_file.time().".{$type}";
+            if (file_put_contents($new_file, base64_decode(str_replace($result[1], '', $base64_image_content)))){
+                //                 echo '新文件保存成功：', $new_file;
+                $res = array('result'=>"/Public/Cover/".date('Ymd',time())."/".time().".{$type}");
+            }else{
+                //                 echo '新文件保存失败';
+                $res = array('error'=>'fail');
+            }
+        }
+         
+        $this->ajaxReturn($res,'JSON');
+    }
